7fcdd63cb68b4996c67217e53d7c4f99a2951b0f
Roman Sypiak <romansipyak@gmail.com>
2020-07-01 09:37:09 +0300

    Add ConfigurationManager to services

---
+++ src/Reenbit.ChuckNorris.API/Extentions/DependencyInjectionConfig.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Reenbit.ChuckNorris.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reenbit.ChuckNorris.API.Extentions
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection RegisterDependencies(this IServiceCollection services)
+        {
+            RegisterInfrastructure(services);
+
+            return services;
+        }
+
+
+        public static void RegisterDataAccess(this IServiceCollection services)
+        {
+        // add uow    
+        }
+
+        public static void RegisterInfrastructure(this IServiceCollection services)
+        {
+            services.AddTransient<IConfigurationManager, ConfigurationManager>();
+        }
+
+    }
+}
