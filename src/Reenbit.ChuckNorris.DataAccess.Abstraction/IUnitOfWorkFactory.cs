0f22af1b8468648e1cd7ff2484ce973a9ec66e8c
Roman Sypiak <romansipyak@gmail.com>
2020-07-01 12:15:16 +0300

    Add Abstractions Base Interfaces for BaseRepository And UOW

---
+++ src/Reenbit.ChuckNorris.DataAccess.Abstraction/IUnitOfWorkFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reenbit.ChuckNorris.DataAccess.Abstraction
+{
+    public interface IUnitOfWorkFactory
+    {
+        IUnitOfWork CreateUnitOfWork();
+    }
+}
