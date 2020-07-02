09ef75171fa996d4444fd64438441708677c823f
Roman Sypiak <romansipyak@gmail.com>
2020-07-02 14:59:19 +0300

    Add references for projects and register services, mappingprofile for JokeDto and repositories

--- src/Reenbit.ChuckNorris.API/Startup.cs
+++ src/Reenbit.ChuckNorris.API/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Reenbit.ChuckNorris.API.Extentions;
 
 namespace Reenbit.ChuckNorris.API
 {
@@ -24,7 +25,10 @@ namespace Reenbit.ChuckNorris.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.RegisterDependencies();
+            services.AddControllers(setupActions => {
+                setupActions.ReturnHttpNotAcceptable = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
