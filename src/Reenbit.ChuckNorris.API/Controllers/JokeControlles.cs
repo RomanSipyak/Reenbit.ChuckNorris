37c720e0682dce0f943c589d958c88eff766f3c5
Roman Sypiak <romansipyak@gmail.com>
2020-07-02 15:00:11 +0300

    add controller for JokeResource

---
+++ src/Reenbit.ChuckNorris.API/Controllers/JokeControlles.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Reenbit.ChuckNorris.Domain.DTOs;
+using Reenbit.ChuckNorris.Services.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Reenbit.ChuckNorris.API.Controllers
+{
+    [ApiController]
+    [Route("api/jokes")]
+    public class JokeControlles : ControllerBase
+    {
+        private readonly IJokeService jokeService;
+
+        public JokeControlles(IJokeService jokeService)
+        {
+            this.jokeService = jokeService;
+        }
+
+        [HttpGet]
+        [Route("random")]
+        public async Task<IActionResult> GetRandomJoke([FromQuery][ModelBinder(BinderType = typeof(ArrayModelBinder))] IEnumerable<string> category)
+        {
+            return Ok(await jokeService.GetRundomJokeAsync(category));
+        }
+    }
+
+    public class ArrayModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (!bindingContext.ModelMetadata.IsEnumerableType)
+            {
+                bindingContext.Result = ModelBindingResult.Failed();
+                return Task.CompletedTask;
+            }
+
+            var value = bindingContext.ValueProvider.GetValue(bindingContext.ModelName).ToString();
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                bindingContext.Result = ModelBindingResult.Success(null);
+                return Task.CompletedTask;
+            }
+
+            var elementType = bindingContext.ModelType.GetTypeInfo().GenericTypeArguments[0];
+            var converter = TypeDescriptor.GetConverter(elementType);
+
+            var values = value.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries)
+                              .Select(x => converter.ConvertFromString(x.Trim()))
+                              .ToArray();
+
+            var typeValues = Array.CreateInstance(elementType, values.Length);
+            values.CopyTo(typeValues, 0);
+            bindingContext.Model = typeValues;
+
+            bindingContext.Result = ModelBindingResult.Success(bindingContext.Model);
+            return Task.CompletedTask;
+        }
+    }
+}
