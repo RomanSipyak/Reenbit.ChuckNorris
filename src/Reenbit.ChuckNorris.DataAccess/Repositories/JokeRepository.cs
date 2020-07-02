4a22be853a23bdc0c7aef647a5d1aa6bf4aeeb56
Roman Sypiak <romansipyak@gmail.com>
2020-07-02 19:49:20 +0300

    delete external lines

--- src/Reenbit.ChuckNorris.DataAccess/Repositories/JokeRepository.cs
+++ src/Reenbit.ChuckNorris.DataAccess/Repositories/JokeRepository.cs
@@ -14,8 +14,6 @@ namespace Reenbit.ChuckNorris.DataAccess.Repositories
     {
         public async Task<Joke> GetByIdWithCategoryIncludeAsync(int id)
         {
-            /* List<Expression<Func<Joke,object>>> ListOfIncludes = new List<Expression<Func<Joke, object>>>() { x => x.JokeCategories };*//*
-            Dictionary<Expression<Func<Joke, object>>, List<Expression<Func<Joke, object>>>> ListOfIncludes = new Dictionary<Expression<Func<Joke, object>>, List<Expression<Func<Joke, object>>>> { x => x.JokeCategories };*/
             return await this.DBSet.Where(jk => jk.Id == id)
                               .Include(jk => jk.JokeCategories)
                               .ThenInclude(jk => jk.Category)
