08a56fa53df1dcce685fa368091155e5b65a5955
Roman Sypiak <romansipyak@gmail.com>
2020-07-02 14:57:04 +0300

    Add JokeDTO

---
+++ src/Reenbit.ChuckNorris.Domain/DTOs/JokeDTOS/JokeDTO.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Reenbit.ChuckNorris.Domain.DTOs
+{
+    public class JokeDTO
+    {
+        public int Id { get; set; }
+
+        public string Value { get; set; }
+
+        public string Url { get; set; }
+        [JsonProperty("icon_url")]//TODO need to be fixed (read abour reflection and config mapping profiles better)
+        public string IconUrl { get; set; }
+
+       // public ICollection<JokeCategory> JokeCategories { get; set; }
+    }
+}
---
+++ src/Reenbit.ChuckNorris.Domain/DTOsProfiles/JokeProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Reenbit.ChuckNorris.Domain.DTOs;
+using Reenbit.ChuckNorris.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reenbit.ChuckNorris.Domain.DTOsProfiles
+{
+    public class JokeProfile : Profile
+    {
+        public JokeProfile()
+        {
+            CreateMap<Joke, JokeDTO>().ReverseMap();
+        }
+    }
+}
