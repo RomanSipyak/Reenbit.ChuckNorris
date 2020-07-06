c83a1f96884a0d851722ec6785c565ccc21196ce
Roman Sypiak <romansipyak@gmail.com>
2020-07-06 15:31:41 +0300

    delete GetJokeDtoByIdWithCategoriesAsync(int from JokeRepository

--- src/Reenbit.ChuckNorris.DataAccess/Repositories/JokeRepository.cs
+++ src/Reenbit.ChuckNorris.DataAccess/Repositories/JokeRepository.cs
@@ -14,24 +14,5 @@ namespace Reenbit.ChuckNorris.DataAccess.Repositories
 {
     public class JokeRepository : EntityFrameworkCoreRepository<Joke, int>, IJokeRepository
     {
-        private readonly IMapper mapper;
-
-        public JokeRepository(IMapper mapper)
-        {
-            this.mapper = mapper;
-        }
-
-        public async Task<JokeDTO> GetJokeDtoByIdWithCategoriesAsync(int id)
-        {
-            var joke = await this.DBSet.Where(j => j.Id == id).FirstOrDefaultAsync();
-            var categoriesForJoke = await this.DbContext.Set<Category>().Where(c => c.JokeCategories.Any(jc => jc.JokeId == id)).ToListAsync();
-            var JokeDtoForReturn = this.mapper.Map<JokeDTO>(joke);
-            if (categoriesForJoke != null)
-            {
-                JokeDtoForReturn.Categories = categoriesForJoke.Select(c => c.Title).ToList();
-            }
-
-            return JokeDtoForReturn;
-        }
     }
 }
