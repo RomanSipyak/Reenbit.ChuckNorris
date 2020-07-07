0ee4d59bf1c619e7ca54bbfa3cc5267d37880c8f
Roman Sypiak <romansipyak@gmail.com>
2020-07-07 13:52:56 +0300

    Add swagger

--- src/Reenbit.ChuckNorris.API/Startup.cs
+++ src/Reenbit.ChuckNorris.API/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using Reenbit.ChuckNorris.API.CustomMiddlewares;
 using Reenbit.ChuckNorris.API.Extentions;
 using Reenbit.ChuckNorris.Domain.DTOsProfiles;
@@ -31,6 +32,12 @@ namespace Reenbit.ChuckNorris.API
         {
             services.RegisterDependencies();
             services.AddAutoMapper(Assembly.GetAssembly(typeof(MappingProfileForDTOs)));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CHUCKNORRIS API", Version = "v1" });
+            });
+
             services.AddControllers(setupActions =>
             {
                 setupActions.ReturnHttpNotAcceptable = true;
@@ -44,6 +51,12 @@ namespace Reenbit.ChuckNorris.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CHUCKNORRIS API");
+            });
 
             app.UseRouting();
 
