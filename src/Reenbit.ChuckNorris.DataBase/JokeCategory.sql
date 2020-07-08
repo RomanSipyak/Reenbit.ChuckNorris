99a4e43e6b38428c5bb1e1a46c2050cf76d0ee67
Roman Sypiak <romansipyak@gmail.com>
2020-07-08 17:45:57 +0300

    delete on Cascade Delete

--- src/Reenbit.ChuckNorris.DataBase/JokeCategory.sql
+++ src/Reenbit.ChuckNorris.DataBase/JokeCategory.sql
@@ -2,7 +2,7 @@
 ( 
     [JokeId] INT NOT NULL,
     [CategoryId] INT NOT NULL,
-    CONSTRAINT [FK_JokeCategory_To_Joke] FOREIGN KEY ([JokeId]) REFERENCES Jokes([Id]) ON DELETE CASCADE,
-    CONSTRAINT [FK_JokeCategory_To_Category] FOREIGN KEY ([CategoryId]) REFERENCES Categories([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_JokeCategory_To_Joke] FOREIGN KEY ([JokeId]) REFERENCES Jokes([Id]),
+    CONSTRAINT [FK_JokeCategory_To_Category] FOREIGN KEY ([CategoryId]) REFERENCES Categories([Id]),
     CONSTRAINT [PK_JokeCategory] PRIMARY KEY ([JokeId], [CategoryId]),
 );
