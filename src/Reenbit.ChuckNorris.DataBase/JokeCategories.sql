1646e41c4cdecf989611ea8ae50d79090e7b27ae
Roman Sypiak <romansipyak@gmail.com>
2020-07-09 11:14:27 +0300

    rename FK_JokeCategory_To_Joke => FK_JokeCategories_To_Joke

--- src/Reenbit.ChuckNorris.DataBase/JokeCategories.sql
+++ src/Reenbit.ChuckNorris.DataBase/JokeCategories.sql
@@ -2,7 +2,7 @@
 ( 
     [JokeId] INT NOT NULL,
     [CategoryId] INT NOT NULL,
-    CONSTRAINT [FK_JokeCategory_To_Joke] FOREIGN KEY ([JokeId]) REFERENCES Jokes([Id]),
-    CONSTRAINT [FK_JokeCategory_To_Category] FOREIGN KEY ([CategoryId]) REFERENCES Categories([Id]),
+    CONSTRAINT [FK_JokeCategories_To_Joke] FOREIGN KEY ([JokeId]) REFERENCES Jokes([Id]),
+    CONSTRAINT [FK_JokeCategories_To_Category] FOREIGN KEY ([CategoryId]) REFERENCES Categories([Id]),
     CONSTRAINT [PK_JokeCategories] PRIMARY KEY ([JokeId], [CategoryId]),
 );
