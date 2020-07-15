bc973442e99e35f6da0fcccb101426674cd1f319
Roman Sypiak <romansipyak@gmail.com>
2020-07-15 21:08:53 +0300

    finish fix top5

--- src/Reenbit.ChuckNorris.DataAccess/Repositories/JokeRepository.cs
+++ src/Reenbit.ChuckNorris.DataAccess/Repositories/JokeRepository.cs
@@ -43,62 +43,21 @@ namespace Reenbit.ChuckNorris.DataAccess.Repositories
 
         public async Task<ICollection<JokeDto>> GetFavoritesJokesTopAsync(int topNumber)
         {
-            /* var userFavoriteIds = await this.DbContext.Set<UserFavorite>().AsQueryable()
-                                                                .GroupBy(uf => uf.JokeId)
-                                                                .OrderByDescending(g => g.Count())
-                                                                .Take(topNumber)
-                                                                .Select(g => g.Key)
-                                                                .Join(this.DbContext.Set<Joke>(),
-                                                                 JokeId => JokeId,
-                                                                 j => j.,
-                                                                 JokeToJokeDtoSelector())
-                                                                .ToListAsync();
-             var favoriteJokes = this.DbContext.Set<Joke>().AsQueryable()
-                                                    .Where(j => userFavoriteIds.Contains(j.Id))
-                                                    .Select(JokeToJokeDtoSelector()).ToList()
-                                                    .OrderBy(jd => userFavoriteIds.IndexOf(jd.Id))
-                                                    .ToList();
-             return favoriteJokes;*/
-
-            /*  var favoriteJokes = await ((from uf in this.DbContext.Set<UserFavorite>()
-                                         group uf by uf.JokeId into g
-                                         orderby g.Count() descending
-                                         join j in this.DBSet on g.Key equals j.Id
-                                         select new JokeDto
-                                         {
-                                             Id = j.Id,
-                                             IconUrl = j.IconUrl,
-                                             Url = j.Url,
-                                             Value = j.Value,
-                                             CreatedAt = j.CreatedAt,
-                                             UpdatedAt = j.UpdatedAt,
-                                             Categories = j.JokeCategories.Select(jc => jc.Category.Title).ToList()
-                                         }).Take(5)).ToListAsync();*/
-
-            /*  var favoriteJokes = ((from uf in this.DbContext.Set<UserFavorite>().Include(uf => uf.Joke)
-                                    group uf.Joke by uf.JokeId into g
-                                    orderby g.Count() descending)).Take(5).ToArray();*/
-
-            /*   var results = this.DbContext.Set<UserFavorite>().Include(uf => uf.Joke).GroupBy(z => z.JokeId, z => z.Joke).Select(g => new
-               {
-                   Aggregate = g.First()
-               }).ToArray();*/
-         
-            var favoriteJokes = ((from j in this.DbContext.Set<Joke>().Include( j => j.UserFavorites)
-                                       orderby j.UserFavorites.Count() descending
-                                       select new JokeDto
-                                       {
-                                           Id = j.Id,
-                                           IconUrl = j.IconUrl,
-                                           Url = j.Url,
-                                           Value = j.Value,
-                                           CreatedAt = j.CreatedAt,
-                                           UpdatedAt = j.UpdatedAt,
-                                           Categories = j.JokeCategories.Select(jc => jc.Category.Title).ToList(),
-                                       })).Take(5).ToSql();
-            return null;
+            var favoriteJokes = await ((from j in this.DbContext.Set<Joke>().Include(j => j.UserFavorites)
+                                        orderby j.UserFavorites.Count() descending
+                                        select new JokeDto
+                                        {
+                                            Id = j.Id,
+                                            IconUrl = j.IconUrl,
+                                            Url = j.Url,
+                                            Value = j.Value,
+                                            CreatedAt = j.CreatedAt,
+                                            UpdatedAt = j.UpdatedAt,
+                                            Categories = j.JokeCategories.Select(jc => jc.Category.Title).ToList(),
+                                        })).Take(5).ToListAsync();
+            return favoriteJokes;
         }
-        
+
         public async Task<ICollection<JokeDto>> FindFavoriteJokesForUser(int userId)
         {
             return await this.DbContext.Set<UserFavorite>().AsQueryable()
@@ -160,25 +119,5 @@ namespace Reenbit.ChuckNorris.DataAccess.Repositories
                 .Select(c => new JokeCategory { CategoryId = c.Id, JokeId = joke.Id }).ToListAsync();
             joke.JokeCategories = joke.JokeCategories.Except(jokeCategoriesForDelete).Union(JokeCategoriesForAdding).ToList();
         }
-    }
-
-    public static class QueryableExtensions
-    {
-        public static string ToSql<TEntity>(this IQueryable<TEntity> query) where TEntity : class
-        {
-            var enumerator = query.Provider.Execute<IEnumerable<TEntity>>(query.Expression).GetEnumerator();
-            var relationalCommandCache = enumerator.Private("_relationalCommandCache");
-            var selectExpression = relationalCommandCache.Private<SelectExpression>("_selectExpression");
-            var factory = relationalCommandCache.Private<IQuerySqlGeneratorFactory>("_querySqlGeneratorFactory");
-
-            var sqlGenerator = factory.Create();
-            var command = sqlGenerator.GetCommand(selectExpression);
-
-            string sql = command.CommandText;
-            return sql;
-        }
-
-        private static object Private(this object obj, string privateField) => obj?.GetType().GetField(privateField, BindingFlags.Instance | BindingFlags.NonPublic)?.GetValue(obj);
-        private static T Private<T>(this object obj, string privateField) => (T)obj?.GetType().GetField(privateField, BindingFlags.Instance | BindingFlags.NonPublic)?.GetValue(obj);
     }
 }
