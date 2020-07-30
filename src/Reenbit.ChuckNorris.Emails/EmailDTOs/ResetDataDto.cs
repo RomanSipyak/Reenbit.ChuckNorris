53cb38260cbc0dcd1448d9ce368b851cd1b98c30
Roman Sypiak <romansipyak@gmail.com>
2020-07-30 16:47:02 +0300

    add blank lines

--- src/Reenbit.ChuckNorris.Emails/EmailDTOs/ResetDataDto.cs
+++ src/Reenbit.ChuckNorris.Emails/EmailDTOs/ResetDataDto.cs
@@ -7,7 +7,9 @@ namespace Reenbit.ChuckNorris.Emails.EmailDTOs
     public class ResetDataDto
     {
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
+
         public string ResetPasswordUrl { get; set; }
     }
 }
